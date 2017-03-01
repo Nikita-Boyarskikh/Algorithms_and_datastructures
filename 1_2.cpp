bf085c584734149a50f5799b81b0d45f17e0c0a6
Nikita Boyarskikh <N02@yandex.ru>
2017-03-01 20:56:43 +0300

    Divided 1st task into 2 functions

--- 1_2.cpp
+++ 1_2.cpp
@@ -11,6 +11,27 @@
  * |  75                    |  3 5 5                    |
  */
 
+// Вычисляет простые числа до заданного number, возвращает количество найденных простых чисел
+// Принимает указатель на буфер для простых чисел размером size_of_buffer, и заполняет его простыми числами
+int calcPrimes(int *result, int size_of_buffer, int number) {
+    assert(result != NULL);
+    int index = 0;
+    for(int i = 2; i*i <= number; i++)
+    {
+        while(number%i == 0)
+        {
+            // Проверка на достаточный размер буфера
+            assert(size_of_buffer > index);
+            number /= i;
+            result[index++] = i;
+        }
+    }
+    if(number > 1) {
+        result[index++] = number;
+    }
+    return index;
+}
+
 int main(int argc, char *argv[])
 {
 
@@ -19,15 +40,23 @@ int main(int argc, char *argv[])
 
     int n = 0;
     assert(cin>>n);
+    assert(n >= 0);
 
-    for(int i = 2; i*i <= n; i++)
-    {
-        while(n%i == 0)
-        {
-            n /= i;
-            cout<<i<<" ";
-        }
+    if(n < 4) {
+        cout<<n;
+        return 0;
     }
 
+    // Достаточный размер буфера под простые числа до заданного
+    int size_of_buffer = n/2 + 1;
+    // Буфер для простых чисел
+    int *primes = new int[size_of_buffer];
+    int prime_count = calcPrimes(primes, size_of_buffer, n);
+
+    for (int i=0; i<prime_count; i++) {
+        cout<<primes[i]<<" ";
+    }
+
+    delete[] primes;
     return 0;
 }
