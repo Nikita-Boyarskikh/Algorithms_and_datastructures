e69517c94a71f0e03ae0b275fb5a4bcab0c863a3
Nikita Boyarskikh <N02@yandex.ru>
2017-03-01 18:05:16 +0300

    Add template.cpp
    Empty C++ programm

---
+++ template.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cassert>
+
+using std::cin;
+using std::cout;
+
+/*
+ * 
+ * 
+ * |  in                    |  out                      |
+ * |------------------------+---------------------------|
+ * |                        |                           |
+ */
+
+int main(int argc, char *argv[])
+{
+    
+
+    return 0;
+}
