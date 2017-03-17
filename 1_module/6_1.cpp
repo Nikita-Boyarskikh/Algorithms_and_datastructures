56c3786b05261d7a538bce714f20a9757b60ab11
Nikita Boyarskikh <N02@yandex.ru>
2017-03-17 14:35:02 +0300

    Add solution for 6th task

--- 1_module/6_1.cpp
+++ 1_module/6_1.cpp
@@ -15,40 +15,78 @@
  * |  7                     |  15                       |
  */
 
-// Запись для компактного хранения вычисленных данных
-struct node {
-    size_t data;
-    node *next;
-}
+// Ограничивающие значение, заданные в условии
+#define MAX_CUBES 200
+
+// Прототип функции, вычисляющей количество возможных "высоких" пирамид, которые можно построить из cubes кубиков.
+// Функция возвращает количество возможных вариантов
+unsigned long long high_piramid_variants(size_t cubes);
 
 int main(int argc, char *argv[])
 {
-
     using std::cin;
     using std::cout;
+    using std::endl;
 
-    size_t N;
+    // Ввод количества кубиков и его валидация
+    int N;
     assert(cin>>N);
     assert(N > 0);
-    assert(N <= 200);
+    assert(N <= MAX_CUBES);
+    // Сохраняем в переменную безопасного типа
+    size_t cubes = N;
 
-    // Аллокация памяти
-    size_t **variants = new size_t*[N];
-    for(size_t i = 0; i < N; i++) {
-        variants[i] = new node;
+    // Вычисляем результат и выводим на экран
+    unsigned long long result = high_piramid_variants(cubes);
+    assert(cout<<result<<endl);
+
+    return 0;
+}
+
+// Функции, вычисляющая количество возможных "высоких" пирамид, которые можно построить из cubes кубиков.
+// Функция возвращает количество возможных вариантов
+unsigned long long high_piramid_variants(size_t cubes)
+{
+    assert(cubes <= MAX_CUBES);
+    unsigned long long result = 0;
+
+    // Выделяем память для сохранения половины таблицы N x N, так как нам для вычислений
+    // понадобится только левая верхняя половина
+    unsigned long long **variants = new unsigned long long *[cubes];
+    assert(variants);
+    for(size_t i = 0; i < cubes; i++) {
+        variants[i] = new unsigned long long [cubes - i];
+        assert(variants[i]);
     }
 
-    size_t result = high_piramid_variants(N, N);
-    assert(cout<<result);
+    // Инициализируем граничные значения
+    for(size_t i = 0; i < cubes; i++) {
+        // Пирамиду любой допустимой ширины из 1 кубика можно построить только одну
+        variants[0][i] = 1;
+        // Пирамиду ширины 1 можно построить только 1 - стольбец из i кубиков
+        variants[i][0] = 1;
+    }
+
+    // Заполняем таблицу, опираясь на которую можно найти ответ на поставленную задачу
+    for(size_t left = 1; left < cubes; left++) {
+        for(size_t width = 1; width + left < cubes; width++) {
+            if(left > width) {
+                variants[left][width] = variants[left][width - 1] + variants[left - width - 1][width];
+            } else {
+                variants[left][width] = variants[left][width - 1];
+            }
+        }
+    }
+
+    for(size_t i = 0; i < cubes; i++) {
+        result += variants[cubes - i - 1][i];
+    }
 
     // Освобождения памяти
-    for(size_t i = 0; i < N; i++) {
-        while(variants[i]->next != std::nullptr) {
-            node *deleting_node = variants[i];
-            variants[i] = variants[i]->next;
-        }
+    for(size_t i = 0; i < cubes; i++) {
+        delete[] variants[i];
     }
     delete[] variants;
 
-    return 0;
+    return result;
 }
