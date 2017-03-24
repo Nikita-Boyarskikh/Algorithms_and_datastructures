cde56bbc6bab551e3cf6a874604b02bf60d0de79
Nikita Boyarskikh <N02@yandex.ru>
2017-03-25 02:39:03 +0300

    Failed try to swap std::string to dynamic char* buffer
    NEEDS TO BE FIXED!!!!!!

--- 1_module/5_1.cpp
+++ 1_module/5_1.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <cassert>
-#include <string>
-#include <stack>
+#include <cstring>
 
 /*
  * СКОБОЧНАЯ ПОСЛЕДОВАТЕЛЬНОСТЬ
@@ -25,12 +24,47 @@
  * |  ]()}[](({}            |  {[]()}[](({}))           |
  */
 
+namespace Stack {
+
+class Exception : public std::exception {};
+class Overflow : public Exception {};
+
+template <class T>
+class Stack {
+  private:
+    unsigned long long size, cur, chunk;
+    T *data;
+  public:
+    Stack();
+    ~Stack();
+    void pop();
+    T top();
+    void push(T data);
+    bool empty();
+};
+
+}
+
+// Исключение, возникающее при прочтении неизвестного символа
+class UnknownSymbol : public std::exception {};
+
+// Функция, дополняющая строку data размером data_size строкой add размером add_size и, в случае необходимости,
+// динамически увеличивающая размер строки data.
+// Функция возвращает указатель на объединённую строку или NULL в случае ошибки.
+char *realloc_strcat(char **data, unsigned long long &data_size, char const * const add, const unsigned long long add_size);
+
 // Прототип функции, дополняющей скобочную последовательность до сбалансированного вида.
-// Функция принимает строку source, содержащую символы '(){}[]' и, возможно, '\n';
-// возвращает строку result, представляющую из себя дополненную скобками слева и справа строку source
+// Функция принимает массив строк source, содержащий строки, состоящие из символов '(){}[]' и, возможно, '\n';
+// возвращает строку result, представляющую из себя дополненные скобками слева и справа объединённые строки source
 // до сбалансированного вида. Функция возвращает длину вычисленной строки.
 // Если балансировка таким способом невозможна, функция вернёт 0.
-size_t complete_breakets_list(const std::string source, std::string &result);
+unsigned long long complete_breakets_list(char const * const source, char ** result);
+
+// Прототип функции получения данных из STDIN
+unsigned long long input_data(char **input);
+
+// Начальный размер динамических буферов
+const unsigned long long CHUNK_SIZE = 64;
 
 int main(int argc, char *argv[])
 {
@@ -38,28 +72,80 @@ int main(int argc, char *argv[])
     using std::cout;
     using std::endl;
 
-    std::string str, result, buf;
 
-    while(std::getline(cin, buf)) {
-        str = str + buf;
-    }
+    // Ввод данных
+    char *input = new char[CHUNK_SIZE];  // Массив входных строк
+    input_data(&input);
 
-    if(str.length() == 0) {
+//    assert(strlen(input) <= 200000);
+
+    if(strlen(input) == 0) {
         return 0;
     }
 
-    assert(str.length() <= 200000);
+    // Дополнение скобочной последовательности
+    char* result;
+    unsigned long long result_size = complete_breakets_list(input, &result); 
 
-    size_t result_length = complete_breakets_list(str, result);
-    if(result_length == 0) {
+    // Проверка на успешность выполнения
+    if(result_size == 0) {
         cout<<"IMPOSSIBLE"<<endl;
         return 0;
     }
-    assert(result_length >= str.length());
 
+    assert(strlen(result) >= strlen(input));
+
+    // Вывод результатов
     cout<<result<<endl;
 
     return 0;
+}
+
+// Функция, дополняющая строку data размером data_size строкой add размером add_size и, в случае необходимости,
+// динамически увеличивающая размер строки data.
+// Функция возвращает указатель на объединённую строку или NULL в случае ошибки.
+char *realloc_strcat(char **data, unsigned long long &data_size, char const * const add, const unsigned long long add_size)
+{
+    // Если данные не умещаются в буфере, расширим его
+    while(strlen(*data) + strlen(add) >= data_size) {
+        // Расширяем буфер в 2 раза
+        char *temp = new char[data_size *= 2];
+        if(temp == NULL) {
+            return NULL;
+        }
+
+        // Копируем данные в новый буфер и удаляем старый буфер
+        if(!strncpy(temp, *data, strlen(*data))) {
+            if(data_size > 0) delete[] *data;
+            return NULL;
+        }
+        if(data_size > 0) delete[] *data;
+        *data = temp;
+    }
+
+    // Добавим данные в буфер
+    *data = strncat(*data, add, strlen(add));
+    return *data;
+}
+
+// Функция получения данных из STDIN
+// Прочитанные данных помещаются в массив массивов по 255 символов - input.
+// Функция возвращает количество прочитанных символов.
+unsigned long long input_data(char **input) {
+    assert(input);
+
+    unsigned long long input_size = CHUNK_SIZE;  // Размер массива входных данных
+    char buf[255];  // Временный буфер для чтения данных
+    unsigned long long input_length = 0;
+
+    while(fgets(buf, 255, stdin)) {
+        input_length += strlen(buf);
+        *input = realloc_strcat(input, input_size, buf, 255);
+    }
+
+    // Проверяем успешность чтения
+    //assert(ferror(stdin));
+    return input_length;
 }
 
 // Функция, дополняющая скобочную последовательность до сбалансированного вида.
@@ -67,11 +153,15 @@ int main(int argc, char *argv[])
 // возвращает строку result, представляющую из себя дополненную скобками слева и справа строку source
 // до сбалансированного вида. Функция возвращает длину вычисленной строки.
 // Если балансировка таким способом невозможна, функция вернёт 0.
-size_t complete_breakets_list(const std::string source, std::string &result)
+unsigned long long complete_breakets_list(char const * const source, char ** result)
 {
-    std::stack<char> check, tail;
+    unsigned long long input_size = strlen(source);
+    unsigned long long result_size = 0;
+    char symbol;
 
-    for(size_t i=0; i<source.length(); i++) {
+    Stack::Stack<char> check, tail;
+
+    for(unsigned long long i=0; i<input_size; i++) {
         switch(source[i]) {
           case '(':
           case '{':
@@ -117,31 +207,105 @@ size_t complete_breakets_list(const std::string source, std::string &result)
             break;
 
           default:
-            throw std::exception();
+            throw UnknownSymbol();
         }
     }
 
     while(!tail.empty()) {
-        result = result + tail.top();
+        symbol = tail.top();
+        *result = realloc_strcat(result, result_size, &symbol, 1);
         tail.pop();
     }
 
-    result = result + source;
+    *result = realloc_strcat(result, result_size, source, input_size);
 
     while(!check.empty()) {
+        // Если данные не умещаются в буфере, расширим его
+        if(strlen(*result) + 1 >= result_size) {
+            // Расширяем буфер в 2 раза
+            char *temp = new char[result_size *= 2];
+            assert(temp);
+
+            // Копируем данные в новый буфер и удаляем старый буфер
+            temp = strncpy(temp, *result, strlen(*result));
+            delete[] result;
+            *result = temp;  // Копируем указатель
+        }
         switch(check.top()) {
           case '(':
-            result = result + ')';
+            symbol = ')';
+            *result = strncat(*result, &symbol, 1);
             break;
           case '{':
-            result = result + '}';
+            symbol = '}';
+            *result = strncat(*result, &symbol, 1);
             break;
           case '[':
-            result = result + ']';
+            symbol = ']';
+            *result = strncat(*result, &symbol, 1);
             break;
         }
         check.pop();
     }
 
-    return result.length();
+    return strlen(*result);
+}
+
+template <class T>
+Stack::Stack<T>::Stack()
+{
+    chunk = CHUNK_SIZE;
+    size = chunk;
+    data = new T[size];
+    assert(data);
+    cur = 0;
+}
+
+template <class T>
+void Stack::Stack<T>::pop()
+{
+    if(cur < 1) throw Overflow();
+    if(size - cur == chunk) {
+        size -= chunk;
+        T *temp = new T[size];
+        assert(temp);
+        assert(memmove(temp, data, size));
+        delete[] data;
+        data = temp;
+        chunk /= 2;
+    }
+    cur--;
+}
+
+template <class T>
+void Stack::Stack<T>::push(T data)
+{
+    this->data[++cur] = data;
+    if(cur == size - 1) {
+        size += chunk;
+        T *temp = new T[size];
+        assert(temp);
+        assert(memmove(temp, this->data, size - chunk));
+	delete[] this->data;
+	this->data = temp;
+        chunk *= 2;
+    }
+}
+
+template <class T>
+bool Stack::Stack<T>::empty()
+{
+    return cur == 0;
+}
+
+template <class T>
+T Stack::Stack<T>::top()
+{
+    return data[cur];
+}
+
+template <class T>
+Stack::Stack<T>::~Stack()
+{
+    delete[] data;
 }
